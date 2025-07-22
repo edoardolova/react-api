e6707740c715bfc20bd4e3dd6e7efc54fb9b281e
Edoardo Lova <edoardolova81@gmail.com>
2025-07-22 16:23:36 +0200

    feat(actors): fetch data and display actors cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 
 export default function Main(){
     const [actressData, setActressData] = useState(null);
+    const [actorsData, setActorsData] = useState(null);
 
 
     useEffect(()=>{
@@ -12,6 +13,14 @@ export default function Main(){
             setActressData(data)
         })
     },[])
+    useEffect(()=>{
+        fetch('https://lanciweb.github.io/demo/api/actors/')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setActorsData(data)
+        })
+    },[])
 
 
     return(
@@ -20,22 +29,39 @@ export default function Main(){
                 {actressData && (
                     actressData.map((actress, index)=>{
                         return(
-                            <>
-                                <div className="col-4 d-flex" key={index}>
-                                    <div class="card h-100">
-                                        <img src={actress.image} class="card-img-top" alt={actress.name}/>
-                                        <div class="card-body">
-                                            <h5 class="card-title mb-3">{actress.name}</h5>
-                                            <p class="card-text mb-2 d-flex"><span className="fw-semibold me-1">Anno di nascita:</span>{actress.birth_year}</p>
-                                            <p class="card-text mb-2 d-flex"><span className="fw-semibold me-1">Nazionalità:</span>{actress.nationality}</p>
-                                            <p class="card-text mb-2 d-flex d-flex"><span className="fw-semibold me-1">Biografia:</span>{actress.biography}</p>
-                                            <p class="card-text mb-2 d-flex"><span className="fw-semibold me-1">Awards:</span>{actress.awards}</p>
-                                        </div>
+                            <div className="col-4 d-flex" key={index}>
+                                <div class="card h-100">
+                                    <img src={actress.image} className="card-img-top" alt={actress.name}/>
+                                    <div className="card-body">
+                                        <h3 className="card-title mb-3">{actress.name}</h3>
+                                        <p className="card-text mb-2 d-flex"><span className="fw-semibold me-1">Anno di nascita:</span>{actress.birth_year}</p>
+                                        <p className="card-text mb-2 d-flex"><span className="fw-semibold me-1">Nazionalità:</span>{actress.nationality}</p>
+                                        <p className="card-text mb-2 d-flex d-flex"><span className="fw-semibold me-1">Biografia:</span>{actress.biography}</p>
+                                        <p className="card-text mb-2 d-flex"><span className="fw-semibold me-1">Awards:</span>{actress.awards}</p>
                                     </div>
                                 </div>
-                            </>
+                            </div>
                         )
-                    }))}
+                    }))
+                }
+                {actorsData && (
+                    actorsData.map((actor, index)=>{
+                        return(
+                            <div className="col-4 d-flex" key={index}>
+                                <div className="card h-100">
+                                    <img src={actor.image} className="card-img-top" alt={actor.name}/>
+                                    <div className="card-body">
+                                        <h3 className="card-title mb-3">{actor.name}</h3>
+                                        <p className="card-text mb-2 d-flex"><span className="fw-semibold me-1">Anno di nascita:</span>{actor.birth_year}</p>
+                                        <p className="card-text mb-2 d-flex"><span className="fw-semibold me-1">Nazionalità:</span>{actor.nationality}</p>
+                                        <p className="card-text mb-2 d-flex d-flex"><span className="fw-semibold me-1">Biografia:</span>{actor.biography}</p>
+                                        <p className="card-text mb-2 d-flex"><span className="fw-semibold me-1">Awards:</span>{actor.awards}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }))
+                }
             </div>
         </>
     )
